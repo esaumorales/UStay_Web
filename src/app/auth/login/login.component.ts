07a5ee570e6150d3f569e6f18e04ec516182e34d
esaumorales <jose.morales@upeu.edu.pe>
2024-10-03 20:46:44 -0500

    Agregar usuario funciona

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,18 +1,40 @@
-import { isPlatformBrowser } from '@angular/common';
-import { Component, Inject, OnInit, PLATFORM_ID, Renderer2 } from '@angular/core';
+import { AsyncPipe, isPlatformBrowser } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { Observable } from 'rxjs';
+
+export class USER {
+  userId: number;
+  userEmail: string;
+  userPassword: string;
+  userName: string;
+
+  constructor() {
+    this.userId = 0;
+    this.userEmail = '';
+    this.userPassword = '';
+    this.userName = '';
+  }
+}
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule, AsyncPipe],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
 })
 export default class LoginComponent implements OnInit {
   signUpButton!: HTMLElement;
   signInButton!: HTMLElement;
   container!: HTMLElement;
+
+  userObj: USER = new USER();
+  http = inject(HttpClient);
+  userList$: Observable<USER[]> = new Observable<USER[]>();
 
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
@@ -31,5 +53,26 @@ export default class LoginComponent implements OnInit {
         this.container.classList.remove('right-panel-active');
       });
     }
+    this.userList$ = this.http.get<USER[]>('http://localhost:3000/userList');
+    this.getUser(); // Llamar a getUser en ngOnInit
+  }
+
+  getUser() {
+    this.http
+      .get<USER[]>('http://localhost:3000/userList')
+      .subscribe((res: USER[]) => {
+        // Procesar la respuesta aquí
+      });
+  }
+  onSaveUser(){
+
+    this.http.get<USER>("http://localhost:3000/createUser").subscribe((res:USER)=>{
+      alert("Usuario Creado")
+
+    })
+    // this.http.post<USER>("http://localhost:3000/createUser", this.userObj).subscribe((res:USER)=>{
+    //   alert("Usuario Creado")
+
+    // })
   }
 }
