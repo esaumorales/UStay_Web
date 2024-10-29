860ba4394f877f526cca72407a89ebe35504ffaa
luis-keny <lucerobalvinluiskeny@gmail.com>
2024-10-29 10:23:43 -0500

    feat: departmental compilation for the departmental card

--- src/app/shared/department-card/department-card.component.ts
+++ src/app/shared/department-card/department-card.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Department } from '../../core/index.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-department-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './department-card.component.html',
   styleUrl: './department-card.component.css'
 })
 export class DepartmentCardComponent {
-
+  @Input({ required: true }) department!: Department;
 }
