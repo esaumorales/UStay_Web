263976eaccc5ccf99144f2e05abdf10bb0e335e6
luis-keny <lucerobalvinluiskeny@gmail.com>
2024-10-28 22:15:32 -0500

    feat: implement offer section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MainLayoutComponent } from '../shared/main-layout/main-layout.component';
+import { OfferComponent } from '../shared/offer/offer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MainLayoutComponent],
+  imports: [MainLayoutComponent, OfferComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
